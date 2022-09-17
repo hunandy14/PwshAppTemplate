28ef121e5bf7421ebcd9714df18826054a960720
Charlotte.Hong <hunandy14@gmail.com>
2022-09-18 00:12:31 +0900

    補上回傳 ExitCode 的代碼

--- RumApp.bat
+++ RumApp.bat
@@ -8,11 +8,11 @@ Set FuncName=Import-Param
 Set Param1='Setting.json'
 Set Param2=-NodeName:'Param1'
 Set Param3=-AutoLoadCsv -TrimCsvValue
-call %Powershell% -C "& {Set-Location '%~dp0'; Import-Module .\'%PwshLib%'; %FuncName% %Param1% %Param2% %Param3%}"
+call %Powershell% -C "& {Set-Location '%~dp0'; Import-Module .\'%PwshLib%'; %FuncName% %Param1% %Param2% %Param3%; Exit $LastExitCode}"
 
 :: 直接執行檔案
 Set PwshApp=App.ps1
-call %Powershell% -C "& {Set-Location '%~dp0'; .\'%PwshApp%';}"
+call %Powershell% -C "& {Set-Location '%~dp0'; .\'%PwshApp%'; Exit $LastExitCode}"
 
 :: 輸出錯誤代碼
 echo %errorlevel%
