bdaebe8bac1e8c2a8f7cf0ae2134628f46c65502
Charlotte.Hong <hunandy14@gmail.com>
2022-09-18 17:23:14 +0900

    主程式新增計時器

--- App.ps1
+++ App.ps1
@@ -1,5 +1,10 @@
 Import-Module .\'unity\Import-Param.ps1'
 
+# 計時開始
+$Date = (Get-Date); $StWh = New-Object System.Diagnostics.Stopwatch; $StWh.Start()
+
+
+
 # 獲取當前批次檔位置
 if ($PSScriptRoot) { $curDir = $PSScriptRoot } else { $curDir = (Get-Location).Path }
 Set-Location $curDir
@@ -8,7 +13,14 @@ Set-Location $curDir
 $Param = (Import-Param 'Setting.json' -NodeName:'Param1' -AutoLoadCsv -TrimCsvValue);
 Write-Host "Excel內容: `n" + $Param.CsvObject
 # 獲取安全密碼
-$PassWd = DecryptPassWord $Param.SecurePassword;
+$PassWd = DecryptPassWord $Param.SecurePWord;
 Write-Host "安全密碼: $PassWd"
+Write-Host ""
+
+
+
+$StWh.Stop(); $Time = "{0:hh\:mm\:ss\.fff}" -f [timespan]::FromMilliseconds($StWh.ElapsedMilliseconds)
+$LogStr  = "[$Date] $Msg" + (", 耗時: $Time")
+Write-Host "[$Date] 開始執行, 耗時 [" -NoNewline; Write-Host $Time -NoNewline -ForegroundColor:DarkCyan; Write-Host "] 執行結束"
 
 Exit 1
