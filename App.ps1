0f1cda7002a2b606ab0bd342099f33fa7cc4fabc
Charlotte.Hong <hunandy14@gmail.com>
2022-10-06 18:21:15 +0900

    全面更新支援讓 pwsh5.1 可執行 utf-8 檔案

--- App.ps1
+++ App.ps1
@@ -2,7 +2,8 @@
 if ($PSScriptRoot) { $curDir = $PSScriptRoot } else { $curDir = (Get-Location).Path }
 Set-Location $curDir
 # 載入函式
-Import-Module .\'unity\Import-Param.ps1'
+# Import-Module .\'unity\Import-Param.ps1'
+Invoke-Expression([Io.File]::ReadAllText((Convert-Path '.\unity\Import-Param.ps1'), [Text.Encoding]::GetEncoding('UTF-8')))
 
 # 計時開始
 $StWh=(StopWatch -Start)
@@ -11,9 +12,11 @@ $StWh=(StopWatch -Start)
 $Param = (Import-Param 'Setting.json' -NodeName:'Param1');
 Write-Host "Excel內容: `n" + $Param.CsvObject
 # 獲取安全密碼
-$PassWd = [Runtime.InteropServices.Marshal]::PtrToStringBSTR([Runtime.InteropServices.Marshal]::SecureStringToBSTR($Param.SecurePWord))
-Write-Host "安全密碼: $PassWd"
-Write-Host ""
+if ($Param.SecurePWord) { 
+    $PassWd = [Runtime.InteropServices.Marshal]::PtrToStringBSTR([Runtime.InteropServices.Marshal]::SecureStringToBSTR($Param.SecurePWord))
+    Write-Host "安全密碼: $PassWd"
+    Write-Host ""
+}
 
 # 輸出LOG
 $Msg    = "設定檔載入完成"
--- RumApp.bat
+++ RumApp.bat
@@ -1,43 +1,57 @@
+::--------------------------------------------------------------------------------------------------------------------------------
 @echo off
 CD "%~dp0"
 :: [版本5: powershell.exe (預設就有)], [版本7: pwsh.exe (額外安裝)]
-Set Powershell=pwsh.exe
+Set Powershell=powershell.exe
 :: 設置pwsh執行權限
 Set Execut=try{Set-ExecutionPolicy -ExecutionPolicy:Bypass -Scope:Process}catch{}
+:: 設置ps1讀取時的Encoding
+Set Encoding=[Text.Encoding]::GetEncoding('UTF-8')
 
 
-echo ----------------------------------------------------------------
+
 :: 方法1: 載入並執行函式
+::--------------------------------------------------------------------------------------------------------------------------------
 Set PwshLib=Unity\Import-Param.ps1
 Set FuncName=Import-Param
 Set Param1='Setting.json'
 Set Param2=-NodeName:'Param1'
 Set Param3=-TrimCsvValue
-call %Powershell% -Nop -C "& {%Execut%; Set-Location '%~dp0'; Import-Module .\'%PwshLib%'; %FuncName% %Param1% %Param2% %Param3%; Exit $LastExitCode}"
-echo ExitCode: %errorlevel%
+@REM call %Powershell% -Nop -C "& {%Execut%; Set-Location '%~dp0'; Import-Module .\'%PwshLib%'; %FuncName% %Param1% %Param2% %Param3%; Exit $LastExitCode}"
+@REM echo ExitCode: %errorlevel%
 
 
+
+:: 方法2: 直接執行主程式1
+::--------------------------------------------------------------------------------------------------------------------------------
 echo ----------------------------------------------------------------
-:: 方法2: 直接執行檔案
 Set PwshApp=App.ps1
-call %Powershell% -Nop -C "& {%Execut%; Set-Location '%~dp0'; .\'%PwshApp%'; Exit $LastExitCode}"
+@REM call %Powershell% -Nop -C "& {%Execut%; Set-Location '%~dp0'; .\'%PwshApp%'; Exit $LastExitCode}"
+@REM echo ExitCode: %errorlevel%
+
+:: 方法2: 直接執行主程式2 (支援UTF-8的PS1檔案)
+Set PwshApp=App.ps1
+call %Powershell% -Nop -C "& {%Execut%; Set-Location '%~dp0'; iex([Io.File]::ReadAllText((Convert-Path '%PwshApp%'),%Encoding%)); Exit $LASTEXITCODE;}"
 echo ExitCode: %errorlevel%
 
 
+
+:: 方法3: 把Powershell寫入同一份Bat檔案 (支援UTF-8的PS1檔案)
+::--------------------------------------------------------------------------------------------------------------------------------
 echo ----------------------------------------------------------------
-:: 方法3: 把Powershell寫入同一份Bat檔案
-set "0=%~f0" & set "1=%~dp0" & call %Powershell% -nop -c "iex (%Execut%; [io.file]::ReadAllText($env:0) -split '[:]PwshScript')[1];"
+set "0=%~f0" & set "1=%~dp0" & call %Powershell% -nop -c "%Execut%; iex([Io.File]::ReadAllText($env:0,%Encoding%) -split '[:]PwshScript')[1];"
 echo ExitCode: %errorlevel%
-
-
 pause
 Exit %errorlevel%
+
+
 
 ::--------------------------------------------------------------------------------------------------------------------------------
 :PwshScript
 #:: script
-Set-Location "$($env:1)"
-.\"App.ps1"
+Set-Location ($env:1)
+$AppFile='App.ps1'; $AppEnc='UTF-8'
+iex([Io.File]::ReadAllText((Convert-Path $AppFile), [Text.Encoding]::GetEncoding($AppEnc)))
 Exit $LastExitCode
 #:: done #:PwshScript
 ::--------------------------------------------------------------------------------------------------------------------------------
