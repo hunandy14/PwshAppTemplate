1d929f4c2b9a68da8e5a01d6f64065f8d9ef8091
Charlotte.Hong <hunandy14@gmail.com>
2022-10-06 16:55:08 +0900

    更新執行權限問題

--- RumApp.bat
+++ RumApp.bat
@@ -2,6 +2,8 @@
 CD "%~dp0"
 :: [版本5: powershell.exe (預設就有)], [版本7: pwsh.exe (額外安裝)]
 Set Powershell=pwsh.exe
+:: 設置pwsh執行權限
+Set Execut=try{Set-ExecutionPolicy -ExecutionPolicy:Bypass -Scope:Process}catch{}
 
 
 echo ----------------------------------------------------------------
@@ -11,20 +13,20 @@ Set FuncName=Import-Param
 Set Param1='Setting.json'
 Set Param2=-NodeName:'Param1'
 Set Param3=-TrimCsvValue
-call %Powershell% -Nop -C "& {Set-Location '%~dp0'; Import-Module .\'%PwshLib%'; %FuncName% %Param1% %Param2% %Param3%; Exit $LastExitCode}"
+call %Powershell% -Nop -C "& {%Execut%; Set-Location '%~dp0'; Import-Module .\'%PwshLib%'; %FuncName% %Param1% %Param2% %Param3%; Exit $LastExitCode}"
 echo ExitCode: %errorlevel%
 
 
 echo ----------------------------------------------------------------
 :: 方法2: 直接執行檔案
 Set PwshApp=App.ps1
-call %Powershell% -Nop -C "& {Set-Location '%~dp0'; .\'%PwshApp%'; Exit $LastExitCode}"
+call %Powershell% -Nop -C "& {%Execut%; Set-Location '%~dp0'; .\'%PwshApp%'; Exit $LastExitCode}"
 echo ExitCode: %errorlevel%
 
 
 echo ----------------------------------------------------------------
 :: 方法3: 把Powershell寫入同一份Bat檔案
-set "0=%~f0" & set "1=%~dp0" & call %Powershell% -nop -c "iex ([io.file]::ReadAllText($env:0) -split '[:]PwshScript')[1];"
+set "0=%~f0" & set "1=%~dp0" & call %Powershell% -nop -c "iex (%Execut%; [io.file]::ReadAllText($env:0) -split '[:]PwshScript')[1];"
 echo ExitCode: %errorlevel%
 
 
