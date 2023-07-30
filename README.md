28232fde21c20d937befde17ce7f1a61d0242968
Charlotte.Hong <hunandy14@gmail.com>
2023-07-30 18:30:55 +0900

    新增解析參數方法

--- README.md
+++ README.md
@@ -2,3 +2,28 @@ PowerShell App 程序樣板
 ====
 
 注意事項：Setting.json 裡面的密碼要自己更新，那個密碼生成的時候有用到當前使用者的UID，換使用者之後檢測道不同會報錯。
+
+拆分參數可以用下面的方式
+
+用匿名函式去讀參數，結果會被自動收集到陣列裡
+
+```ps1
+$Arguments = @(& {return $args} $env:2)
+```
+
+另一個方法是偷 PowerShell 內建獲取函式的方法，直接轉線程的物件出來用
+
+```ps1
+$Arguments = '"C:\List 1.csv" -Table CHG.CHG.CHG_M01'
+
+$ScriptBlock = {
+    param(
+        [Parameter(Position = 0, ParameterSetName = "", Mandatory)]
+        [string] $Path,
+        [Parameter(ParameterSetName = "")]
+        [string] $Table
+    ) $PSBoundParameters
+}; $Arguments = Invoke-Expression "& `$ScriptBlock $Arguments"
+
+$Arguments
+```
--- RunPwshTest.bat
+++ RunPwshTest.bat
@@ -3,14 +3,17 @@
 :: 方法1: PowerShell 5 運行
 set "0=%~f0"& set "1=%~dp0"& set "2=%*"& set PwshScript=([Io.File]::ReadAllText($env:0,[Text.Encoding]::GetEncoding('UTF-8')) -split '[:]PwshScript')
 powershell -nop -exec Bypass "(%PwshScript%[3])|iex; Exit $LastExitCode"
+
 :: 方法1: PowerShell 7 運行 (有nop的條件下無法使用 UTF8 以外的編碼載入)
 @REM set "0=%~f0"& set "1=%~dp0"& set "2=%*"& set PwshScript=([Io.File]::ReadAllText($env:0,[Text.Encoding]::GetEncoding('UTF-8')) -split '[:]PwshScript')
 @REM pwsh -nop -exec Bypass -c "(%PwshScript%[3])|iex; Exit $LastExitCode"
+
 :: 方法3: PowerShell 7 運行 (把檔案寫到temp裡避開方法2的問題)
 @REM set "0=%~f0"& set "1=%~dp0"& set "2=%*"& set PwshScript=([Io.File]::ReadAllText($env:0,[Text.Encoding]::Default) -split '[:]PwshScript')
 @REM set PsFile=($env:temp+'\a.ps1')& set path=%path%;C:\Program Files\PowerShell\7
 @REM powershell -nop -exec Bypass "(%PwshScript%[3])|Out-File %PsFile% utf8"
 @REM pwsh -nop -exec Bypass -c "([Io.File]::ReadAllText(%PsFile%,[Text.Encoding]::Default))|iex; Exit $LastExitCode"
+
 echo ExitCode: %errorlevel%
 Exit /b %errorlevel%
 
