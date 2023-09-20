035c2c8b5d1cd58c4008e9d5c340805439993e2f
Charlotte.Hong <hunandy14@gmail.com>
2023-09-21 00:11:34 +0900

    添加返回參數

--- Hybrid.cmd
+++ Hybrid.cmd
@@ -1,3 +1,4 @@
-@(set "0=%~f0"^)#) & set "1=%*" & powershell -nop -c "iex([io.file]::ReadAllText($env:0))" & exit /b
+@(set "0=%~f0"^)#) & set "1=%*" & setlocal enabledelayedexpansion & powershell -nop -c "iex([io.file]::ReadAllText($env:0));$Host.SetShouldExit($LastExitCode);Exit $LastExitCode" & exit /b !errorlevel!
 Write-Host "by PSVersion::" $PSVersionTable.PSVersion
 Write-Host "Command is '$env:0 $env:1'"
+Exit -7
