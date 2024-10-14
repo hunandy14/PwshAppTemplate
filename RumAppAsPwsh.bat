43606e0a9d459282d0f9f6d7f4203c60451acbf3
Charlotte.Hong <hunandy14@gmail.com>
2024-10-14 17:15:17 +0900

    合併更新後的檔案

--- RumAppAsPwsh.bat
+++ RumAppAsPwsh.bat
@@ -1,46 +1,53 @@
 @echo off
 
-set "0=%~f0"& set "1=%~dp0"& set "2=%*"& set PwshScript=([Io.File]::ReadAllText($env:0,[Text.Encoding]::GetEncoding('UTF-8')) -split '[:]PwshScript')
-
 :: 方法1: PowerShell 5 運行
-powershell -nop "(%PwshScript%[2])|iex; Exit $LastExitCode"
+set "0=%~f0"& set "1=%~dp0"& set "2=%*"& set PwshScript=([Io.File]::ReadAllText($env:0,[Text.Encoding]::GetEncoding('UTF-8')) -split '[:]PwshScript')
+powershell -nop -exec Bypass "(%PwshScript%[3])|iex; Exit $LastExitCode"
 
 :: 方法1: PowerShell 7 運行 (有nop的條件下無法使用 UTF8 以外的編碼載入)
-@REM pwsh -nop -c "(%PwshScript%[2])|iex; Exit $LastExitCode"
+@REM set "0=%~f0"& set "1=%~dp0"& set "2=%*"& set PwshScript=([Io.File]::ReadAllText($env:0,[Text.Encoding]::GetEncoding('UTF-8')) -split '[:]PwshScript')
+@REM pwsh -nop -exec Bypass -c "(%PwshScript%[3])|iex; Exit $LastExitCode"
 
 :: 方法3: PowerShell 7 運行 (把檔案寫到temp裡避開方法2的問題)
-@REM set "0=%~f0"& set "1=%~dp0"& set PwshScript=([Io.File]::ReadAllText($env:0,[Text.Encoding]::Default) -split '[:]PwshScript')
+@REM set "0=%~f0"& set "1=%~dp0"& set "2=%*"& set PwshScript=([Io.File]::ReadAllText($env:0,[Text.Encoding]::Default) -split '[:]PwshScript')
 @REM set PsFile=($env:temp+'\a.ps1')& set path=%path%;C:\Program Files\PowerShell\7
-@REM powershell -nop "(%PwshScript%[2])|Out-File %PsFile% utf8"
-@REM pwsh -nop -c "([Io.File]::ReadAllText(%PsFile%,[Text.Encoding]::Default))|iex; Exit $LastExitCode"
+@REM powershell -nop -exec Bypass "(%PwshScript%[3])|Out-File %PsFile% utf8"
+@REM pwsh -nop -exec Bypass -c "([Io.File]::ReadAllText(%PsFile%,[Text.Encoding]::Default))|iex; Exit $LastExitCode"
 
-echo ExitCode: %errorlevel%& pause
-Exit %errorlevel%
-
-
+echo ExitCode: %errorlevel%
+Exit /b %errorlevel%
 
 
-
-:PwshScript#:: script1
+:PwshScript#:: Script1 Main
 #:: --------------------------------------------------------------------------------------------------------------------------------
 Write-Host "by PSVersion::" $PSVersionTable.PSVersion
-$AppFile='App.ps1'; $AppEnc='UTF-8'
-iex([Io.File]::ReadAllText($AppFile, [Text.Encoding]::GetEncoding($AppEnc)))
+"RunTest" |WriteLog ".\log\WriteLog.log" -ErrorAction Stop
 
 
-:PwshScript#:: script2
+:PwshScript#:: Script2 Function
+#:: --------------------------------------------------------------------------------------------------------------------------------
+Invoke-RestMethod "raw.githubusercontent.com/hunandy14/WriteLog/master/WriteLog.ps1"|Invoke-Expression
+
+
+:PwshScript#:: Script3 Execute
 #:: --------------------------------------------------------------------------------------------------------------------------------
 try {
-    Set-ExecutionPolicy Bypass Process -Force
     Set-Location ($env:1); [IO.Directory]::SetCurrentDirectory(((Get-Location -PSProvider FileSystem).ProviderPath)); $Arguments = $env:2
+    ([Io.File]::ReadAllText($env:0,[Text.Encoding]::GetEncoding('UTF-8')) -split '[:]PwshScript')[2]|iex
     ([Io.File]::ReadAllText($env:0,[Text.Encoding]::GetEncoding('UTF-8')) -split '[:]PwshScript')[1]|iex
 } catch {
-    #$Msg = $_.Exception.Message
-    $Msg = $Error|Out-String
-    $LogStr = "[$((Get-Date).Tostring('yyyy/MM/dd HH:mm:ss.fff'))] $Msg"
-    Write-Host $LogStr -ForegroundColor:Red
-    $LogStr >> "log\exception.log"
+    $LogPath = ".\log\exception.log"
+    $Enc = New-Object System.Text.UTF8Encoding $True
+    $FormatType = "yyyy/MM/dd HH:mm:ss.fff"
+    $Msg = ($Error |Out-String)
+    $Date = "[$((Get-Date).Tostring($FormatType))]"
+    Write-Host $Date -BackgroundColor:Red
+    Write-Host " $Msg" -ForegroundColor:Red
+    if (!(Test-Path $LogPath)) { New-Item $LogPath -ItemType:File -Force |Out-Null }
+    [IO.File]::AppendAllText($LogPath, "$Date`r`n$Msg`r`n", $Enc)
+    Exit 1
 }
+
 
 :PwshScript#:: End
 #:: --------------------------------------------------------------------------------------------------------------------------------
