b91d3a067f3bb98b5fb0e7d997fc16a62f195e7c
Charlotte.Hong <hunandy14@gmail.com>
2024-10-14 18:26:57 +0900

    刪除不必要檔案

--- Hybrid.cmd
+++
@@ -1,4 +0,0 @@
-@(set "0=%~f0"^)#) & set "1=%*" & setlocal enabledelayedexpansion & powershell -nop -c "iex([io.file]::ReadAllText($env:0));$Host.SetShouldExit($LastExitCode);Exit $LastExitCode" & exit /b !errorlevel!
-Write-Host "by PSVersion::" $PSVersionTable.PSVersion
-Write-Host "Command is '$env:0 $env:1'"
-Exit -7
