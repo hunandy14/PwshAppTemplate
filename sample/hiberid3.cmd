e7221ca0a794db8352a703390438ac38c00d99c3
Charlotte.Hong <hunandy14@gmail.com>
2024-10-25 00:20:51 +0900

    修正參數包含括號的問題

--- sample/hiberid3.cmd
+++ sample/hiberid3.cmd
@@ -1,4 +1,4 @@
 @(set +=^)#)& set "1=%*"& powershell "iex('#'+[io.file]::ReadAllText('%~f0'))"& exit /b
-$argslist = @(if($env:1){cmd /c "for %i in ($env:1) do @echo %~i"}else{$args})
+$argslist = @(if($env:1){cmd /V:ON /c "set x=$env:1 & for %i in (!x!) do @echo %i"}else{$args})
 Write-Host "by PSVersion::" $PSVersionTable.PSVersion
 Write-Host "Argslist: [$($argslist -join ', ')]"
