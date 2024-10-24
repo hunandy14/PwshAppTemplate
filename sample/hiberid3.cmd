5eb5af8e2477d2cc0a5e564a2384ef9d040ba822
Charlotte.Hong <hunandy14@gmail.com>
2024-10-25 00:30:28 +0900

    優化指令

--- sample/hiberid3.cmd
+++ sample/hiberid3.cmd
@@ -1,4 +1,4 @@
 @(set +=^)#)& set "1=%*"& powershell "iex('#'+[io.file]::ReadAllText('%~f0'))"& exit /b
-$argslist = @(if($env:1){cmd /V:ON /c "set x=$env:1 & for %i in (!x!) do @echo %i"}else{$args})
+$argslist = @(if($env:1){cmd /V:ON /c "for %i in (!1!) do @echo %i"}else{$args})
 Write-Host "by PSVersion::" $PSVersionTable.PSVersion
 Write-Host "Argslist: [$($argslist -join ', ')]"
